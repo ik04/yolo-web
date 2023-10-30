64f65e3e5389431413fcd5f898b64b2d5dbdaba1
ik04 <ishaankhurana123@gmail.com>
2023-10-31 04:24:17 +0530

    feat; init functions for handle start and handle stop

--- components/modelDisplay.jsx
+++ components/modelDisplay.jsx
@@ -1,10 +1,24 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Navbar } from "./navbar";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import Image from "next/image";
 
 export const ModelDisplay = () => {
+  const [isStart, setIsStart] = useState(false);
+  const [toggleFeed, setToggleFeed] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); //* for file upload shit
+  const handleStartFeed = async () => {
+    setIsStart(true);
+    setToggleFeed(true);
+  };
+  const handleStopFeed = async () => {
+    // api call
+    setToggleFeed(false);
+    setIsStart(false);
+  };
+
   return (
     <div className="h-screen bg-main flex flex-col">
       <Navbar />
@@ -13,11 +27,28 @@ export const ModelDisplay = () => {
           <h1 className="text-highlight font-base text-[50px] text-center">
             Yolo V5 Nano-Ultralytics
           </h1>
-          <div className="display rounded-[20px] h-[630px] w-[1140px] bg-main2 "></div>
+          <div className="display rounded-[20px] h-[630px] w-[1140px] bg-main2 ">
+            {toggleFeed && (
+              <img
+                src={"http://localhost:5000/video_feed"}
+                alt="Network Error"
+                className="w-full h-full rounded-[20px]"
+              />
+            )}
+          </div>
           <div className="buttons mt-5 flex items-center m-3 space-x-10">
-            <div className="live w-[461px] h-[80px] cursor-pointer flex justify-center items-center bg-main rounded-full border-highlight border text-highlight hover:bg-highlight hover:text-white duration-200 transition-all">
-              <p className="font-base text-[25px]">Live Feed</p>
-            </div>
+            {!isStart ? (
+              <div
+                onClick={handleStartFeed}
+                className="live w-[461px] h-[80px] cursor-pointer flex justify-center items-center bg-main rounded-full border-highlight border text-highlight hover:bg-highlight hover:text-white duration-200 transition-all"
+              >
+                <p className="font-base text-[25px]">Live Feed</p>
+              </div>
+            ) : (
+              <div className="live w-[461px] h-[80px] cursor-pointer flex justify-center items-center bg-main rounded-full border-red-700 border text-red-700 hover:bg-red-700 hover:text-white duration-200 transition-all">
+                <p className="font-base text-[25px]">Stop Feed</p>
+              </div>
+            )}
             <Label
               htmlFor="video"
               className="live w-[461px] h-[80px] cursor-pointer flex justify-center items-center bg-main rounded-full border-highlight border text-highlight hover:bg-highlight hover:text-white duration-200 transition-all"
